edcb7e155dcfb39fa78f7cd1552c49722757686d
Darius Clark <darius@shouthost.org>
2014-02-26 17:12:02 -0500

    Implemented ForgePerms Managers

--- src/main/java/modifyworld2/Modifyworld2.java
+++ src/main/java/modifyworld2/Modifyworld2.java
@@ -1,10 +1,13 @@
 package modifyworld2;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 
+import forgeperms.api.*;
 import net.minecraftforge.common.Configuration;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartedEvent;
 import cpw.mods.fml.common.network.NetworkMod;
@@ -21,9 +24,28 @@ import modifyworld2.handlers.VehicleListener;
 @NetworkMod(clientSideRequired=false, serverSideRequired=true)
 public class Modifyworld2 
 {
-	@Mod.Instance("Modifyworld2")
+    public IPermissionManager permManager;
+    public IChatManager chatManager;
+    public IEconomyManager economyManager;
+	@Instance("Modifyworld2")
 	public static Modifyworld2 instance;
-	
+
+
+    public void initPermManager() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<?> forgePerms = Class.forName("forgeperms.ForgePerms");
+        permManager = (IPermissionManager) forgePerms.getMethod("getPermissionManager").invoke(null);
+    }
+
+    public void initChatManager() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<?> forgePerms = Class.forName("forgeperms.ForgePerms");
+        chatManager = (IChatManager) forgePerms.getMethod("getChatManager").invoke(null);
+    }
+
+    public void initEcoManager() throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Class<?> forgePerms = Class.forName("forgeperms.ForgePerms");
+        economyManager = (IEconomyManager) forgePerms.getMethod("getEconomyManager").invoke(null);
+    }
+
 	public static String MOD_NAME = "Modifyworld2";
 	protected BaseListener[] listeners = new BaseListener[]
 	{
@@ -47,6 +69,22 @@ public class Modifyworld2
     @EventHandler
 	public void modsLoaded(FMLServerStartedEvent var1)
 	{
+        try {
+            initPermManager();
+            initChatManager();
+            initEcoManager();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
 		for (BaseListener l : listeners)
 			l.load();
 		
